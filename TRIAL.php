86df9f04f0de7a6a215e2e716ca93da12873cec5
Joseph Maliza <josephmaliza55@gmail.com>
2025-01-17 06:24:18 +0800

    update

--- TRIAL.php
+++ TRIAL.php
@@ -70,19 +70,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['editProfile'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-
-    <title>NowUKnow</title>
-</head>
-
 <style>
     .custom-card {
         width: 100%;
@@ -160,6 +147,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['editProfile'])) {
         }
     }
 </style>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+    <title>NowUKnow</title>
+</head>
 
 <body>
     <div class="container">
